53062aae5bccbf2da6e473941cfa35f1daa249b0
mended <ekrogstad@gmail.com>
2023-01-17 19:15:28 +0100

    remove /

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
-    target: 'node',
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public'),
